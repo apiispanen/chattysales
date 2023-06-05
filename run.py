2ddb8d49a33317a789adf3f58e552a9b72537a5f
Andrew Piispanen <apiispanen1@babson.edu>
2023-06-05 02:04:54 -0400

    init for streamlit

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 import openai
+from config import API_KEY
+
+# make a streamlit chatbot
+# https://discuss.streamlit.io/t/how-to-make-a-chatbot-in-streamlit/3140/2
+
 
